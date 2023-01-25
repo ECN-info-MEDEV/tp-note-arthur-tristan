b62a1a82be8fb7c830fff0daf49361fb07a3224a
Arthur200111 <arthur.huet01@gmail.com>
2023-01-26 00:28:12 +0100

    hello world with test, jar and junit

---
+++ src/main/java/org/centrale/project/App.java
@@ -0,0 +1,18 @@
+package org.centrale.project;
+
+/**
+ * Classe hello world
+ * 
+ * @author Arthur
+ */
+public class App {
+
+    /**
+     * juste Hello World
+     * 
+     * @param args param de main
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World !");
+    }
+}
---
+++ src/test/java/org/centrale/project/AppTest.java
@@ -0,0 +1,12 @@
+package org.centrale.project;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+    @Test
+    public void testMain() {
+        assertEquals(0, 0);
+    }
+}
