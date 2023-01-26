1497b7ff5b65ecaf8e166976c4cf299e06188b3d
Arthur200111 <arthur.huet01@gmail.com>
2023-01-26 08:49:00 +0100

    change Partie

--- src/main/java/org/centrale/mastermind/App.java
+++ src/main/java/org/centrale/mastermind/App.java
@@ -1,23 +1,7 @@
 package org.centrale.mastermind;
 
-/**
- * Classe hello world
- * 
- * @author Arthur
- */
 public class App {
-
-    Code codeATrouver;
-    int nombreTDJ;
-    Joueur j1;
-    Joueur j2;
-
-    /**
-     * juste Hello World
-     * 
-     * @param args param de main
-     */
     public static void main(String[] args) {
-        System.out.println("Hello World !");
+        Partie p = new Partie();
     }
 }
---
+++ src/main/java/org/centrale/mastermind/Partie.java
@@ -0,0 +1,14 @@
+package org.centrale.mastermind;
+
+/**
+ * Classe hello world
+ * 
+ * @author Arthur
+ */
+public class Partie {
+
+    Code codeATrouver;
+    int nombreTDJ;
+    Joueur j1;
+    Joueur j2;
+}
